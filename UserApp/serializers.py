a2b261ba4a5177c1872269bbe5159213c7e56556
Ossarce <est.arceguzman@gmail.com>
2025-03-22 22:42:33 -0300

    Se crean los serializadores para los modelos User, Category, Product y SalesHistory

---
+++ UserApp/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from UserApp.models import User, Category, Product, SalesHistory
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class SaleHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SalesHistory
+        fields = '__all__' 
