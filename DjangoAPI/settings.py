93f4fd38f2dc8e22ab6f0575cc34713ca0e80d9a
Ossarce <est.arceguzman@gmail.com>
2025-03-23 19:06:34 -0300

    fix: Se añade middleware de corsheaders

--- DjangoAPI/settings.py
+++ DjangoAPI/settings.py
@@ -55,6 +55,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 # ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
